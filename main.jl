460f2cb88137b5bb71abaed30fb0c19e547ec9af
tmarku123 <110036672+tmarku123@users.noreply.github.com>
2024-10-27 12:23:38 +0000

    Update main.jl

--- main.jl
+++ main.jl
@@ -1,49 +1,49 @@
 include("functions.jl");
 
 ## Define domain ##
-x_max = 200;
-z_max = 200;
-no_layers = 7;
+x_max = 400;
+z_max = 400;
+no_layers = 5;
 scaling_factor = 1;
 domain = create_domain(x_max,z_max,no_layers,scaling_factor);
 
 ## Define Simulations parameters ## 
-dt = 0.1;
-tmax = 50;
+dt = 0.25;
+tmax = 100.0;
 time_vector = collect(0:dt:Int(round(tmax)));
 parameters = Parameters(time_vector);
 no_rays = 100;
 
 ## Set Initial Conditions ## 
-position = [z_max/2,x_max/2];
-amplitude = 25.0;
+pos = [z_max/2,x_max/2];
+amp = 10^2;
 
 ## Single Ray ## 
-#angle = 10.0;
-#initial_conditions = set_initial_conditions(position,angle,amplitude);
-#rays = initialise_rays(no_rays,initial_conditions);
-#simulation = initialise_simulation(parameters,domain,rays);
-#simulation = compute_ray_simulation(simulation::Simulation);
-#animation = animate_rays(simulation.data,domain,10)
+# Run this at 0 degrees to get a good no_rays to run for multiple case 
+angle = 0;
+initial_conditions = set_initial_conditions(pos,angle,amp);
+rays = initialise_rays(no_rays,initial_conditions);
+simulation = initialise_simulation(parameters,domain,rays);
+simulation = compute_ray_simulation(simulation::Simulation);
+#animation = animate_rays(simulation.data,domain,7,no_layers)
+
+no_rays = get_number_of_rays_used(simulation)
 
 ## Multiple Rays (Form a wavefront) ## 
-dθ = 0.25;
+dθ = 0.5;
 angles = collect(0:dθ:360)[1:end-1];
 simulations = Vector{Simulation}(undef,size(angles,1));
 for i = 1:size(angles)[1] 
-    initial_conditions = set_initial_conditions(position,angles[i],amplitude);
+    angle = Float64(angles[i])
+    initial_conditions = set_initial_conditions(pos,angle,amp);
     rays = initialise_rays(no_rays,initial_conditions);
     simulation = initialise_simulation(parameters,domain,rays);
     simulation = compute_ray_simulation(simulation::Simulation);
     simulations[i] = simulation
 end
 
-wavefronts = obtain_wavefronts(simulations)
-animate_wavefronts(simulations,wavefronts,8)
+wavefronts = obtain_wavefronts(simulations);
 
-
-
-
-
+darkmode_animate_wavefronts(simulations,wavefronts,8,no_layers)
 
 
